e827d99a7ea7ef31a33d0310ad3b2f569d70f4b7
SUFIYAN SHAIKH <softwaremastar0010@gmail.com>
2024-07-03 19:46:42 +0530

    Add files via upload

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable use for Println
+	var name = "SUFIYAN SHAIKH"
+	fmt.Println(name)
+
+	var age = 18
+	fmt.Println(age)
+
+	var height = 5.7342
+	fmt.Println(height)
+
+	//variable use for Printf
+
+	fmt.Printf("Name: %s\n", name)
+	fmt.Printf("Age: %d\n", age)
+	fmt.Printf("Height: %.2f\n", height)
+}
