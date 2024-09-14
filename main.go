835388e2c2337cdb16656521bbe844e18b56152c
SUFIYAN SHAIKH <softwaremastar0010@gmail.com>
2024-09-14 18:47:27 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("HELLO WORLD")
-	fmt.Println("SUFIYAN SHAIKH")
+	fmt.Println("Learn Slice for Go language ")
+
+	numbers := []int{1, 2, 3}
+	numbers = append(numbers, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+
+	fmt.Println("number is:", numbers)
+	fmt.Printf("numbers has data type is: %T\n", numbers)
+	fmt.Println("length of Numbers is:", len(numbers))
+
 }
